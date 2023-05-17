40934fc102feecd17f742c6957988d8101c16b2d
masktar <110052237+masktar@users.noreply.github.com>
2023-05-17 23:59:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "ttran aiu",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Ttran aiu`,
+    btnIntro: '^^HiHi^^',
+    title: 'Tui có cutti honggg zậy 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Có á <33',
+    btnNo: 'Không nha :3',
+    question: 'Vậy tại sao tui lại cuttiii',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tại Ttran cuti đáng iu nhức còn tson trẻ trâu :3333',
+    mess: 'Mình biết mà, mình lúc nào chả cutiii còn bạn thì shao bằng mình, bạn tủi l😏😏',
+    messDesc: 'Hẹn ngày tái xuất giang hồ :33',
+    btnAccept: 'Mãi yêu 😗',
+    messLink: 'facebook.com/buontaite' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
